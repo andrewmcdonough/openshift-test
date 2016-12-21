f9c18277a0e4e0d519af29639b043efde3d8731b
Andrew McDonough <andrew@andrewmcdonough.co.uk>
2016-12-21 11:37:43 +0000

    Use mongodb

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require "sinatra"
+require "mongo"
+require "pry"
 
 class App < Sinatra::Application
   get "/" do
-    "openshift: #{ENV["APP_GREETING"]}"
+    mongodb_uri = ENV["MONGODB_URI"]
+    output = "openshift: #{ENV["APP_GREETING"]}"
+    client = Mongo::Client.new(mongodb_uri)
+    output += "Mongo databases: #{client.list_databases}"
+    output
   end
 end
