488cd853b483648be0879372e592348aa86412ae
Andrew McDonough <andrew@andrewmcdonough.co.uk>
2016-12-21 13:53:06 +0000

    Add clock

---
+++ clock.rb
@@ -0,0 +1,13 @@
+require 'clockwork'
+module Clockwork
+  handler do |job|
+    puts "Running #{job}"
+  end
+
+  # handler receives the time when job is prepared to run in the 2nd argument
+  # handler do |job, time|
+  #   puts "Running #{job}, at #{time}"
+  # end
+
+  every(10.seconds, 'frequent.job')
+end
