bbaaa7f997429330549ac65ffc3200d7679f7e60
Bence Monus <crome@moronia.hu>
2016-12-22 12:29:09 +0000

    change db config variable

--- app.rb
+++ app.rb
@@ -4,8 +4,8 @@ require "pry"
 
 class App < Sinatra::Application
   get "/" do
-    mongodb_uri = ENV["MONGODB_URI"]
-    output = "openshift: #{ENV["APP_GREETING"]}"
+    mongodb_uri = ENV["DATABASE_URI"]
+    output = "openshift: #{ENV["APP_GREETING"]}<br />"
     client = Mongo::Client.new(mongodb_uri)
     output += "Mongo collectionss: #{client.collections}"
     output
